cef2214644c127b067b01e1978bc2ea125664c29
fuzzsa <78082481+fuzzsa@users.noreply.github.com>
2021-03-31 14:27:20 +0200

    Update harness_blk_char.c

--- harness_blk_char.c
+++ harness_blk_char.c
@@ -1,98 +1,11 @@
-#define _GNU_SOURCE
-#include <time.h>
-#include <dlfcn.h>
-#include <elf.h>
-#include <errno.h>
-#include <sys/personality.h>
-#include <fcntl.h>
-#include <link.h>
-#include <lkl.h>
-#include <lkl_host.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <pthread.h>
 #include "fuzz_interface.h"
 #include "util.h"
 
-static pthread_mutex_t ready_mutex = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t ready_cond = PTHREAD_COND_INITIALIZER;
-static int unload_module = 0;
-static int mod_loaded = 0;
-
-int done_cb(void) {
-  pthread_mutex_lock(&ready_mutex);
-  unload_module = 1;
-  pthread_cond_signal(&ready_cond);
-  pthread_mutex_unlock(&ready_mutex);
-  return 0;
-}
-
-static void trigger_irqs_virtio(void) {
-  for(int qidx=0; qidx<nqueues; qidx++) {
-    lkl_virtio_send_to_queue(fuzz_dev_handle, qidx);
-  }
-}
-
-static void trigger_irqs_pci(void) {
-  long n_irq;
-  int irqs[512];
-  n_irq = lkl_fuzz_get_requested_irqs(irqs);
-  for(long i=0; i<n_irq; i++) {
-     lkl_sys_fuzz_trigger_irq(irqs[i]);
-  }
-}
-
-void* thread_done_cb(void *arg)
-{
-  int err;
-  while(true) {
-     pthread_mutex_lock(&ready_mutex);
-     while(!unload_module) {
-        pthread_cond_wait(&ready_cond, &ready_mutex);
-     }
-     pthread_mutex_unlock(&ready_mutex);
-     if(__atomic_exchange_n(&mod_loaded, 0, __ATOMIC_SEQ_CST)) {
-        do {
-           err = lkl_sys_uninit_loaded_module(this_module);
-           if(err!=0) {
-              trigger_irqs_virtio();
-              trigger_irqs_pci();
-           }
-        } while(err);
-     }
-     unload_module = 0;
-  }
-  return NULL;
-}
-
-static pthread_t tid;
-void start_thread_done_cb(void) {
-  pthread_create(&tid, NULL, &thread_done_cb, NULL);
-}
-
-static int irq_cb(void *arg) {
-  if(lkl_fuzz_has_waiters() || lkl_fuzz_wait_for_wait(1)) {
-    if(devtype==1) { // VIRTIO
-      trigger_irqs_virtio();
-    }
-    trigger_irqs_pci();
-    return 1;
-  }
-  return 0;
-}
-
 int mod_init(void) {
-  if(n_request_irqs>0) {
-     start_irqthread_cb(irq_cb);
+  if(n_request_irqs!=0) {
+     start_irqthread_default();
   }
-  lkl_fuzz_set_done_callback(done_cb);
-  start_thread_done_cb();
+  start_thread_done_default();
   return 0;
 }
 
@@ -124,7 +37,6 @@ static int clean_devnodes(int n_dn, int *fds) {
 
 static int write_sth(int n_dn, int *fds) {
    char buf[64];
-   lkl_fuzz_get_n(buf, 64);
    for(int i=0;  i<n_dn; i++) {
       if(fds[i] > 0) {
          lkl_sys_write(fds[i], buf, 64);
@@ -147,42 +59,26 @@ int mod_fuzz(const uint8_t *data, size_t size) {
   int err = 0, ret = 0;
   int fds[LKL_FUZZ_MAX_DEVT];
   int n_dn;
-  request_irqs(-1);
-  lkl_fuzz_set_buf((uint8_t*)data, size);
 
-  err = lkl_sys_init_loaded_module(this_module);
+  start_fuzz(data, size);
+  err = init_module();
   if(err!=0) {
     goto out_noinit;
   }
-  __atomic_store_n (&mod_loaded, 1, __ATOMIC_SEQ_CST);
 
-  if(devtype==1) trigger_irqs_virtio();
-  trigger_irqs_pci();
   n_dn = make_devnodes(fds);
   if(n_dn > 0) {
      write_sth(n_dn, fds);
      read_sth(n_dn, fds);
      clean_devnodes(n_dn, fds);
   }
-  if(__atomic_exchange_n(&mod_loaded, 0, __ATOMIC_SEQ_CST)) {
-     do {
-        err = lkl_sys_uninit_loaded_module(this_module);
-        if(err!=0) {
-           trigger_irqs_virtio();
-           trigger_irqs_pci();
-        }
-     } while(err!=0);
-  }
+  trigger_one_irq();
+
+  uninit_module();
 
 out_noinit:
-  while(unload_module) {
-    struct timespec sleepValue = {0};
-    sleepValue.tv_nsec = DRAIN_DELAY_NS;
-    nanosleep(&sleepValue, NULL);
-  }
-  cancel_irqs();
-  lkl_fuzz_unset_buf();
-  update_hwm();
+  end_fuzz();
   return ret;
 }
+
 
